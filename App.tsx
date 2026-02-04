e29e1990fcfcf53dfb748b361df7b8bf8d7f5f9c
PanuRonkainen <A2502581@edu.lapinamk.fi>
2026-02-04 23:27:14 +0200

    viikko 4 palautettu

--- App.tsx
+++ App.tsx
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { SwipeListView } from 'react-native-swipe-list-view';
+import TodoToggle from './components/TodoToggle';
+import TodoInput from './components/TodoInput';
+import { Item } from './data/listData';
 
-export default function App() {
+const STORAGE_KEY = 'TODO_LIST_ITEMS';
+
+export default function App () {
+  const [items, setItems] = useState<Item[]>([]);
+
+  //Load items
+  useEffect(() => {
+    (async () => {
+      try {
+        const json = await AsyncStorage.getItem(STORAGE_KEY);
+        if (json) setItems(JSON.parse(json));
+       } catch (e) {
+        //todo error
+       }
+    })();
+  }, []);
+  //Save items
+  useEffect(() => {
+    AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(items));
+  }, [items]);
+
+  const addItem = (name: string) => {
+    setItems(prev => [
+      ...prev,
+      { id: Date.now().toString(), name, done: false },
+    ]);
+  };
+
+  const toggleItem = (id: string) => {
+    setItems(prev =>
+      prev.map(item =>
+        item.id === id ? { ...item, done: !item.done } : item
+      )
+    );
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Todo list</Text>
+      <TodoInput onAdd={addItem}/>
+      <SwipeListView
+        data={items}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <TodoToggle item={item} onToggle={toggleItem} />
+        )}
+        renderHiddenItem={() => <View style={styles.rowBack} />}
+        rightOpenValue={-75}
+        disableRightSwipe
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 60,
+    paddingHorizontal: 16,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  rowBack: {
+    backgroundColor: '#ddd',
+    flex: 1,
+    alignItems: 'flex-end',
     justifyContent: 'center',
+    paddingRight: 20,
   },
 });
